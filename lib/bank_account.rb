de4076868d282b32d1a9b4d439b5d116010a7430
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-05-14 13:44:01 -0400

    first bank account push

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+    attr_reader :name
+    attr_accessor :balance, :status
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+    def deposit(amount)
+        self.balance += amount
+    end
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        end
+    end
 end
+
